9aa7632a68f18c79da15e5fbbdbef29c02856a4b
horai <hiroyasu.horai@gmail.com>
2020-03-28 21:47:31 +0900

    [add] NAS encoder.

---
+++ encoding/nas/nas.go
@@ -0,0 +1,23 @@
+// Copyright 2019-2020 hhorai. All rights reserved.
+// Use of this source code is governed by a MIT license that can be found
+// in the LICENSE file.
+
+// Package nas is implementation for non-access stratum (NAS) procedure
+// in the 5GS Sytem.
+// document version: 3GPP TS 24.501 v16.3.0 (2019-12)
+package nas
+
+import (
+/*
+	"encoding/binary"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"math/bits"
+	"strconv"
+
+	"github.com/hhorai/gnbsim/encoding/per"
+*/
+)
+
