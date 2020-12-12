18e1112e6daccb4bff37cea3fd504e486da8f26e
hhorai <hiroyasu.horai@gmail.com>
2020-12-13 00:50:45 +0900

    [add][gnbsim] gnbsim initial commit

---
+++ gnbsim_netlink.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"fmt"
+	"github.com/vishvananda/netlink"
+	"net"
+)
+
+func AddTunnel(name string) (tun *netlink.Tuntap, err error) {
+
+	tun = &netlink.Tuntap{
+		LinkAttrs: netlink.LinkAttrs{Name: name},
+		Mode:      netlink.TUNTAP_MODE_TUN,
+		Flags:     netlink.TUNTAP_DEFAULTS | netlink.TUNTAP_NO_PI,
+		Queues:    1,
+	}
+
+	if err = netlink.LinkAdd(tun); err != nil {
+		err = fmt.Errorf("failed to add tun device[%s]: %s", name, err)
+		return
+	}
+
+	if err = netlink.LinkSetUp(tun); err != nil {
+		err = fmt.Errorf("failed to up tun device[%s]: %s", name, err)
+		return
+	}
+	return
+}
+
+func AddIPv4Address(ifName string, ip net.IP, masklen int) (err error) {
+
+	link, err := netlink.LinkByName(ifName)
+	if err != nil {
+		return err
+	}
+
+	addrs, err := netlink.AddrList(link, netlink.FAMILY_ALL)
+	if err != nil {
+		return err
+	}
+
+	netToAdd := &net.IPNet{
+		IP:   ip,
+		Mask: net.CIDRMask(masklen, 32),
+	}
+
+	var addr netlink.Addr
+	found := false
+	for _, a := range addrs {
+		if a.Label != ifName {
+			continue
+		}
+		found = true
+		if a.IPNet.String() == netToAdd.String() {
+			// It has already set the IP address.
+			return
+		}
+		addr = a
+	}
+
+	if !found {
+		err = fmt.Errorf("interface[%s] not found", ifName)
+		return
+	}
+
+	addr.IPNet = netToAdd
+	if err := netlink.AddrAdd(link, &addr); err != nil {
+		return err
+	}
+	return
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module gnbsim
+
+require (
+	github.com/vishvananda/netlink v1.1.1-0.20200603190747-5400e006d43d
+)
